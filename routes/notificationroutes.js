c6dc41e5cc6e395b7ba05efd3f389e4bde817e25
devhulk <gerald.yerden@icloud.com>
2017-06-15 08:02:32 -0400

    added npm curl and modifying notification route

--- routes/notificationroutes.js
+++ routes/notificationroutes.js
@@ -1,6 +1,7 @@
 const Dispatch = require('../dispatch')
 const express = require('express')
 const mongoose = require('mongoose')
+const curl = require('curl')
 let Node = require('../node')
 class NotificationRoutes {
 
@@ -10,6 +11,8 @@ class NotificationRoutes {
          let node = new Node(req.body)
           if (node.type == 'sms') {
             //This is where you need to call flow API
+            //params:(url, options, function(err,response,body){})
+            curl.post('localhost:3000/flow/sms')
             Dispatch.notification(node) 
           }
           node.markModified(node)
